5c1a91e877a3ef58829893e3b04c8c2c9f9a7505
Paul <privat@Pauls-MacBook-Pro.local>
2019-03-24 14:14:06 +0100

    add logs for error

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,6 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
+    console.log("Error .map is not a fucntion:");
+    console.log(this.state.seenIndexes);
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
