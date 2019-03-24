66d21e26ea998d0c8c44b49af29973d440fc2517
Paul <privat@Pauls-MacBook-Pro.local>
2019-03-24 14:43:31 +0100

    a

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@ docker build -t ahuevote/mulit-client:latest -t ahuevote/multi-client:$SHA -f ./
 docker build -t ahuevote/mulit-server:latest -t ahuevote/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ahuevote/multi-worker:latest -t ahuevote/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-echo "pushing to docker hub..."
+echo "#### pushing to docker hub... ####"
 docker push ahuevote/multi-client:latest
 docker push ahuevote/multi-server:latest
 docker push ahuevote/multi-worker:latest
 
-echo "pushing to docker hub with SHA..."
+echo "#### pushing to docker hub with SHA... ####"
 docker push ahuevote/multi-client:$SHA
 docker push ahuevote/multi-server:$SHA
 docker push ahuevote/multi-worker:$SHA
 
-echo "apply deployment files"
+echo "#### apply deployment files ####"
 kubectl apply -f kubernetes
 kubectl set image deployments/express-deployment express=ahuevote/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ahuevote/multi-client:$SHA
